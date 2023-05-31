4dc76589affd45fb30269f60cfbaf1db8b85926b
Konstantin.Filyurin <kf@item.expert>
2023-05-31 11:27:16 +0300

    First commit

---
+++ AwesomeTaskTracker/AuthentificationService/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AuthentificationService.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
